d81a5b2cb8603a681d40eed4b9f30665197dbc51
Noman-auditor <nomankhan2023bu@.com>
2026-05-23 02:34:33 +0600

    Fix duplicate independent_browser folder path in workflow

--- independent_browser/app/src/main/java/com/custom/browser/MainActivity.java
+++ independent_browser/app/src/main/java/com/custom/browser/MainActivity.java
@@ -9,23 +9,23 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     
+<<<<<<< HEAD
     // Rust ব্যাকএন্ড লাইব্রেরি (.so) লোড করা
+=======
+>>>>>>> 7296fce (Fix webkit import typo in MainActivity)
     static {
         System.loadLibrary("browser_core");
     }
 
-    // Rust নেটিভ ফাংশন ইন্টারফেস
     public native String getCryptoStatus();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // ডাইনামিকলি লেআউট আইডি লোড করা
         int layoutId = getResources().getIdentifier("activity_main", "layout", getPackageName());
         setContentView(layoutId);
 
-        // ওয়েবভিউ সেটআপ
         int webViewId = getResources().getIdentifier("webview", "id", getPackageName());
         WebView webView = (WebView) findViewById(webViewId);
         
@@ -38,7 +38,6 @@ public class MainActivity extends Activity {
             webView.loadUrl("https://www.google.com");
         }
 
-        // Rust ইঞ্জিনের কানেকশন চেক ও টোস্ট মেসেজ
         try {
             String status = getCryptoStatus();
             Toast.makeText(getApplicationContext(), "Core: " + status, Toast.LENGTH_LONG).show();
